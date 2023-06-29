ae24dedb7d51f8485ae4605361251f8dc8ac3a69
SAHA-YJB <pkomom123@naver.com>
2023-06-29 10:04:56 +0900

    week3시험커밋

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState, useEffect, useRef } from "react";
+import { v4 as uuidv4 } from "uuid";
 
 function App() {
+  const [todo, setTodo] = useState([]);
+  const [text, setText] = useState("");
+  const inputRef = useRef(null);
+
+  useEffect(() => {
+    inputRef.current.focus()
+  }, [text]);
+
+  const valueChange = (e) => {
+    setText(e.target.value);
+    console.log(text);
+  };
+
+  const addTodoBtn = () => {
+    const newTodo = { id: uuidv4(), text };
+    setTodo([...todo, newTodo]);
+    console.log(newTodo);
+    setText("");
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="inner">
+        <input value={text} onChange={(e) => valueChange(e)} ref={inputRef} />
+        <button onClick={addTodoBtn}>추가하기</button>
+      </div>
+      <h1>Todo List</h1>
+
+      <div className="todo-container">
+        {todo.map((item) => {
+          return (
+            <div className="todo-box" key={item.id}>
+              <div>{item.text}</div>
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 }
